4f36af5c30719fda9110e16d322422df1de523b2
juusotahtinen <juuso.tahtinen88@hotmail.com>
2021-04-08 15:37:39 +0300

    Update HelloAppEngine.java

--- src/main/java/HelloAppEngine.java
+++ src/main/java/HelloAppEngine.java
@@ -1,32 +1,32 @@
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-import dao.DaoAnswers;
-
-
-@WebServlet(
-    name = "HelloAppEngine",
-    urlPatterns = {"/hello"}
-)
-public class HelloAppEngine extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private DaoAnswers dao = null; 
-	
-    
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-
-    response.setContentType("text/plain");
-    response.setCharacterEncoding("UTF-8");
-
-
-
-  }
-}
+//import java.io.IOException;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//import dao.DaoAnswers;
+//
+//
+//@WebServlet(
+//    name = "HelloAppEngine",
+//    urlPatterns = {"/hello"}
+//)
+//public class HelloAppEngine extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//	private DaoAnswers dao = null; 
+//	
+//    
+//  @Override
+//  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+//      throws IOException {
+//
+//    response.setContentType("text/plain");
+//    response.setCharacterEncoding("UTF-8");
+//
+//
+//
+//  }
+//}
